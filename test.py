6a95223db5b91e0290ffd11fc562bfcff4ecc22e
Ola <vadimyhlin42ve@gmail.com>
2020-10-03 10:15:43 +0200

    Проверка библиотеки pynput

--- test.py
+++ test.py
@@ -1,3 +1,34 @@
+
 import pyautogui
-a = pyautogui.confirm('Choose language', 'Change', ('Ru', 'En'))
-print(a)
+import time 
+
+# a = pyautogui.confirm('Choose language', 'Change', ('Ru', 'En'))
+# print(a)
+
+# from pynput import keyboard
+
+# def on_activate_h():
+#     print('<ctrl>+<alt>+h pressed')
+
+# def on_activate_i():
+#     print('<ctrl>+<alt>+i pressed')
+
+# with keyboard.GlobalHotKeys({
+#         '<ctrl>+<alt>+h': on_activate_h,
+#         '<ctrl>+<alt>+i': on_activate_i}) as h:
+# 	h.join()
+from pynput import keyboard
+
+def on_activate():
+    print('Global hotkey activated!')
+
+def for_canonical(f):
+    return lambda k: f(l.canonical(k))
+
+hotkey = keyboard.HotKey(
+    keyboard.HotKey.parse('<ctrl>+<alt>+h'),
+    on_activate)
+with keyboard.Listener(
+        on_press=for_canonical(hotkey.press),
+        on_release=for_canonical(hotkey.release)) as l:
+    l.join()
