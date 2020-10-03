ae4caff11b57af753335a388a27117b0a9b95885
Ola <vadimyhlin42ve@gmail.com>
2020-10-03 16:08:43 +0200

    Добавил функцию exit_from_prog и горячие клавиши на функции translating, choose_translating, exit_from_prog

--- main.py
+++ main.py
@@ -2,24 +2,27 @@ import keytranslater
 import pyperclip
 import pyautogui
 import time
+from pynput import keyboard
 
-def translating(kbt):
+def translating():
+	kbt = keytranslater.KeyboardTranslater(pyperclip.paste())
 	pyperclip.copy(kbt.translater())
 
-def choose_translating(kbt):
+def choose_translating():
+	kbt = keytranslater.KeyboardTranslater(pyperclip.paste())
 	lang = pyautogui.confirm('Выбери язык', 'Язык', ('Ru', 'En'))
 	pyperclip.copy(kbt.choose_translate(lang))
 
-def replase_word():	
-	kt = keytranslater.KeyboardTranslater(pyperclip.paste())
-	
-	translating(kt)
-	choose_translating(kt)
+def exit_from_prog():
+	exit()
 
-	time.sleep(.5)
-	pyautogui.hotkey('alt', 'tab')
-	time.sleep(.6)
-	pyautogui.hotkey('ctrl', 'v')
+def replase_word():
+	
+	with keyboard.GlobalHotKeys({'<ctrl>+<alt>+q': translating,'<ctrl>+<alt>+l': choose_translating, '<ctrl>+e': exit_from_prog	}) as h:
+		time.sleep(.6)
+		pyautogui.hotkey('ctrl', 'v')
+		h.join()
 
 if __name__ == '__main__':
 	replase_word() 
+
